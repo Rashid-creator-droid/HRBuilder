4756b656b8f60664d97cdfb2de777fe7884af86d
Olga Karpova <tordvist@yandex.ru>
2024-03-19 12:09:27 +0300

    Add default value to Payment model

--- models/payment.py
+++ models/payment.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import Date, Integer
+from sqlalchemy import BigInteger, Date, Integer
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
@@ -9,8 +9,9 @@ from .enums import PaymentMethod
 
 
 class Payment(Base):
-    numb_employees: Mapped[int] = mapped_column(Integer)
+    num_employees: Mapped[int] = mapped_column(Integer, default=1)
     start_date: Mapped[datetime.date] = mapped_column(Date)
     end_date: Mapped[datetime.date] = mapped_column(Date)
-    num_recruiters: Mapped[int] = mapped_column(Integer)
+    num_recruiters: Mapped[int] = mapped_column(Integer, default=1)
     payment_method: Mapped[PaymentMethod]
+    reward_per_employee: Mapped[int] = mapped_column(BigInteger)
